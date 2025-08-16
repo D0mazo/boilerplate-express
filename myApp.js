13b78e53716d2dd4ee6510e5d0ec850daceb1cf9
D0mazo <dominykas.simanavicius@proton.me>
2025-08-16 19:26:31 +0300

    hello express

--- myApp.js
+++ myApp.js
@@ -4,13 +4,12 @@ let app = express();
 // Log "Hello World" to the console
 console.log("Hello World");
 
+// Respond with "Hello Express" when the root URL ("/") is requested
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
+
 module.exports = app;
-
-
-
-
-
-
 
 
 
